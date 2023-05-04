31c1b23e81563512765c0c6259935a97542fe100
Subash100 <bajagainsubas10@gmail.com>
2023-05-04 06:11:51 +0545

    new

---
+++ variables_types.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract v_riable{
+
+}
+
+//three main types of  variables :booleans , Integers , StringsS
+
+//types:boolean - keyword :bool - value :true/false
+//integer - keyword - uint - value - singed and unsigned integers of varying sizes
+//string - keyword -string - data values that are made up of ordered sequences of characters
