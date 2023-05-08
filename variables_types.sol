e7221b005816d6247a6c2b387a064a21782f2415
Subash100 <bajagainsubas10@gmail.com>
2023-05-08 17:53:54 +0545

    variable

--- variables_types.sol
+++ variables_types.sol
@@ -2,7 +2,16 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract v_riable{
+contract learnVariables{
+   uint chocolateBar = 10;
+   uint storeOwner = 300;
+   bool lieDetector = true;
+   string errorMessageText= 'error! there been a mistake!';
+
+   //exercise
+   uint wallet = 500;
+   bool spend = false;
+   string notifySpend = "you have spent money";
 
 }
 
@@ -11,3 +20,4 @@ contract v_riable{
 //types:boolean - keyword :bool - value :true/false
 //integer - keyword - uint - value - singed and unsigned integers of varying sizes
 //string - keyword -string - data values that are made up of ordered sequences of characters
+//string format inside '' or ""
