91b9cbe202d5a60a7d3f154333ddfdd9d3a69811
Subash100 <bajagainsubas10@gmail.com>
2023-05-28 11:28:18 +0545

    date and time

---
+++ assertTimeAndDate.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+contract learnDate
+{
+    //seconds,minutes,hours,days and weeks
+
+    function time() public
+    {
+        assert(1 minutes==60 seconds);
+        assert(1 hours==60 minutes);
+        assert(24 hours==1 days);
+        assert(7 days==1 weeks);
+    }
+}
